e005ea8f339cc5b7e5f0bbbd6d16caf7b12be8ef
Armine8 <armisha15@mail.ru>
2022-11-09 11:23:43 +0300

    Task #5. Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,27 +2,27 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public static void printHourglassOfGivenSize(int height) {
-        for(int i = 1; i <= height; i++) {
+        for(int i = 1; i <= height/2+1; i++) {
             for (int k = 1; k <= i - 1; k++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= height - i + 1; j++) {
-                System.out.print("8 ");
+            for (int j = 1; j <= height+2-i-i; j++) { //5 , 3 , 1
+                System.out.print("8");
             }
             System.out.println();
         }
-        for(int i = height-1; i >= 1; i--) {
+        for(int i = (height/2+1)-1; i >= 1; i--) {
             for (int k = 1; k <= i - 1; k++) {
                 System.out.print(" ");
             }
-            for (int j=i; j<=height; j++)
-            {
-                System.out.print("8 ");
+            for (int j=i; j<height+2-i; j++) //3, 5,7
+            {              // i = 2 ---1    4/6
+                System.out.print("8");
             }
             System.out.println();
         }
     }
     public static void main(String[] args){
-        printHourglassOfGivenSize(7);
+        printHourglassOfGivenSize(5);
     }
 }
